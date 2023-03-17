fdb30d2c9ff1a370b1c5d5f18710d6fe62996932
Henrik <henrik.hakansson@fcc.chalmers.se>
2023-03-17 10:16:14 +0100

    Add optional output group for function components

--- sakkara/model/math_op.py
+++ sakkara/model/math_op.py
@@ -3,7 +3,7 @@ from abc import ABC
 from typing import Any
 
 from sakkara.model.base import ModelComponent
-from sakkara.model.function import FunctionComponent
+from sakkara.model.function.base import FunctionComponent
 
 
 class MathOpBase(ModelComponent, ABC):
@@ -36,4 +36,4 @@ class MathOpBase(ModelComponent, ABC):
         return FunctionComponent.math_op(operator.truediv, other, self)
 
     def __neg__(self):
-        return FunctionComponent(operator.neg, self)
+        return FunctionComponent(operator.neg, None, self)
--- tests/components/test_function_component.py
+++ tests/components/test_function_component.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 import pymc as pm
+import pytensor.tensor as pt
 
 from sakkara.model import DistributionComponent as DC, build, f_, DataComponent
 
@@ -115,3 +116,20 @@ def test_tensor_fct(simple_df):
     assert tuple(map(str, y.representation.get_groups())) == ('time', 'sensor')
     assert pm.draw(y.variable).shape == (5, 4)
     assert all(pm.draw(y.variable)[i, j] == 1 + i * (j + 1) for j in range(4) for i in range(5))
+
+
+@pytest.mark.usefixtures('simple_df')
+def test_output_group(simple_df):
+    x = DC(pm.Uniform, 'x', lower=np.array([(s + 1) * np.arange(5) for s in range(4)]),
+           upper=np.array([(s + 1) * np.arange(5) for s in range(4)]), group=('sensor', 'time'))
+
+    y = f_(pt.sum, output_group='sensor')(x, axis=1)
+
+    _ = build(simple_df, y)
+
+    assert tuple(map(str, y.representation.get_groups())) == ('sensor',)
+    assert pm.draw(y.variable).shape == (4,)
+
+    assert all(pm.draw(y.variable)[i] == 5 * 4 * (i + 1) // 2 for i in range(4))
+
+
